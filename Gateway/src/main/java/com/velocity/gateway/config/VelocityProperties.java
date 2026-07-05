36ac852b069ae9293848ff349bb6a2515f06a379
kanchanraiii <kanchanrai2307@gmail.com>
2026-07-05 19:33:09 +0530

    refactor: removing routes hardcoding and use routing table instead

--- Gateway/src/main/java/com/velocity/gateway/config/VelocityProperties.java
+++ Gateway/src/main/java/com/velocity/gateway/config/VelocityProperties.java
@@ -1,19 +1,22 @@
 package com.velocity.gateway.config;
 
+import com.velocity.gateway.model.Route;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(prefix = "velocity")
+import java.util.List;
+
 @Component
+@ConfigurationProperties(prefix = "velocity")
 public class VelocityProperties {
-    private String userServiceUrl;
-    public String getUserServiceUrl() {
-        return userServiceUrl;
+
+    private List<Route> routes;
+
+    public List<Route> getRoutes() {
+        return routes;
     }
 
-    public void setUserServiceUrl(String userServiceUrl) {
-        this.userServiceUrl = userServiceUrl;
+    public void setRoutes(List<Route> routes) {
+        this.routes = routes;
     }
-
 }
