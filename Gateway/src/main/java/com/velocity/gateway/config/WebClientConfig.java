8849527f0ebaf4666779d254815a1cabc6998f70
kanchanraiii <kanchanrai2307@gmail.com>
2026-07-05 18:07:30 +0530

    add: gateway-service

--- Gateway/src/main/java/com/velocity/gateway/config/WebClientConfig.java
+++ Gateway/src/main/java/com/velocity/gateway/config/WebClientConfig.java
@@ -1,4 +1,14 @@
 package com.velocity.gateway.config;
 
-public class WebClient {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().build();
+    }
 }
--- Gateway/src/main/java/com/velocity/gateway/controller/GatewayController.java
+++ Gateway/src/main/java/com/velocity/gateway/controller/GatewayController.java
@@ -1,4 +1,25 @@
 package com.velocity.gateway.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController
 public class GatewayController {
+
+    private final WebClient webClient;
+
+    public GatewayController(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return webClient
+                .get()
+                .uri("http://localhost:8081/users")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 }
